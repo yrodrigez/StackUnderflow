a36b25f2ba4438ca4e8b3716f3d97d870c4eff4f
Yago Rodriguez <y_rodrigez@hotmail.com>
2015-11-25 20:09:38 +0100

    un comentario rapido

---
+++ Model/postDAO.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author
+ * Date: 15/11/2015
+ * Time: 12:27
+ */
+require_once(__DIR__ . "../core/PDOConnection.php");
+require_once(__DIR__ . "post.php");
+class postMapper {
+
+    private $db;
+
+    public function __construct()
+    {
+        $this->db= PDOConnection::getInstance();
+    }
+
+    /**
+     * @param $userId
+     * @return PDOStatement
+     */
+    public function getAllUserPosts(
+        $userId
+    ) {
+        $stmt = $this->db->prepare(
+            "SELECT * FROM posts WHERE user_id= ?"
+        );
+        if($stmt->execute(array(
+            $userId
+        ))) {
+            $usuarios = array();
+            for($i= 0 ; $i < $stmt->rowCount(); $i++){
+                array_push(
+                    $usuarios,
+                    new Usuario(
+                        $stmt["id"],
+                        $stmt["username"],
+                        $stmt["password"],
+                        $stmt["tipo"],
+                        $stmt["email"],
+                        $stmt["descripcion"],
+                        $stmt["foto"]
+                    )
+                );
+            }
+            return $usuarios;
+        }
+        return false;
+    }
+
+    /**
+     * @param $size
+     * @return array
+     */
+    public function getHotPosts(
+        $size
+    ) {
+        $stmt = $this->db->prepare(
+          "SELECT * FROM  posts ORDER BY numvisitas"
+        );
+        if( $stmt->rowCount() > $size ){
+            $ret= array();
+            for($i = 0; $i<$size; $i++){
+                array_push($ret, $this->fill($stmt["id"]));
+            }
+            return $ret;
+        }else{
+            $ret= array();
+            for($i = 0; $i < $stmt->rowCount(); $i++){
+                $ret[$i]= $stmt[$i];
+            }
+            return $ret;
+        }
+    }
+
+    /**
+     * @param $post
+     * @return bool
+     */
+    public function save(
+        $post
+    ) {
+        $stmt = $this->db->prepare(
+            "INSERT INTO posts(
+                            titulo,
+                            user_id,
+                            cuerpo,
+                            created,
+                            modified,
+                            contestada
+                            ) VALUES
+                            (?,?,?,?,?)"
+        );
+        return $stmt->execute(
+            array(
+                $post->getTitulo(),
+                $post->getIdUsuario(),
+                $post->getCuerpo(),
+                $post->getFechaCreacion(),
+                $post->getFechaModificacion(),
+                $post->getContestada()
+            )
+        );
+
+    }
+
+    public function edit(
+        $post
+    ) {
+        $stmt = $this->db->prepare(
+          "UPDATE posts SET titulo= ?, cuerpo= ?, created= ?, contestada= ? WHERE id= ?"
+        );
+        $stmt->execute(
+            $post->getTitulo(),
+            $post->getCuerpo(),
+            $post->getFechaCreacion()
+        );
+    }
+
+    /**
+     * @param $idPost
+     * @return Post
+     */
+    public function fill(
+        $idPost
+    ) {
+        $stmt = $this->db->prepare(
+            "SELECT * FROM posts WHERE id= ?"
+        );
+
+        if ($stmt->execute(array($idPost))){
+            if($stmt->rowCount() > 0){
+                $fillData= $stmt->fetch(PDO::FETCH_ASSOC);
+                return new Post(
+                    $fillData["titulo"],
+                    $fillData["contestada"],
+                    $fillData["cuerpo"],
+                    $fillData["numvisitas"],
+                    $fillData["created"],
+                    $fillData["user_id"]
+                );
+            }
+        }
+
+    }
+
+    /**
+     *
+     * @param $idPost
+     * @return bool
+     *
+     */
+    public function delete(
+        $idPost
+    ) {
+        $stmt= $this->db->prepare(
+            "DELETE * FROM post WHERE id= ?"
+        );
+        return $stmt->execute(array($idPost));
+    }
+
+}
